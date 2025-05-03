c203f5e163f9ded641b68b7179a81fb8e0a8a44a
Amit Ghiya <amit.ghiya@ameri100.com>
2025-05-03 18:01:07 -0400

    changed service name

--- limits-services/src/main/java/com/training/microservices/limits_services/config/Configuration.java
+++ limits-services/src/main/java/com/training/microservices/limits_services/config/Configuration.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties(value = "limit-service")
+@ConfigurationProperties(value = "limits-service")
 public class Configuration {
 
     private Integer minimum;
